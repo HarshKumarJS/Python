08c00692777863711cc198c45671ddbe37a76259
Harsh Kumar <harsh.kumar9118b@gmail.com>
2025-09-12 13:52:02 +0530

    program to input two floating numbers and print their average

---
+++ Basics/average_float_numbers.py
@@ -0,0 +1,11 @@
+# Program to input two floating numbers and print their average
+
+# Taking input from the user (floating-point numbers)
+a = float(input("Enter a: "))
+b = float(input("Enter b: "))
+
+# Calculating the average
+average = (a + b) / 2
+
+# Printing the result
+print("The average of a and b is:", average)
