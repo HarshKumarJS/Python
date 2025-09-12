77cf347670111fd35fedc165c109d0cfd157008e
Harsh Kumar <harsh.kumar9118b@gmail.com>
2025-09-12 13:21:30 +0530

    program to input two numbers and print their sum

---
+++ Basics/add_two_nums_from_user_input.py
@@ -0,0 +1,11 @@
+# Program to input two numbers and print their sum
+
+# Taking input from user and converting it into integer
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+
+# Calculating the sum
+sum = a + b
+
+# Printing the result
+print("The sum of a and b is:", sum)
