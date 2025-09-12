94c5d855a93e40f189322721bd68b6b02409859c
Harsh Kumar <harsh.kumar9118b@gmail.com>
2025-09-12 13:35:37 +0530

    program to input side of a square and print its area

---
+++ Basics/input_square_side_and_find_its_area.py
@@ -0,0 +1,10 @@
+# Program to input side of a square and print its area
+
+# Taking input from the user (side length of the square in cm)
+a = float(input("Enter side of a square (in cm): "))
+
+# Calculating area (side × side)
+area = a * a
+
+# Printing the result with unit cm²
+print("The area of square is:", area, "cm²")
