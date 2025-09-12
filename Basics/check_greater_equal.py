7abfaaaac9a443d62639615b0728ff544249d003
Harsh Kumar <harsh.kumar9118b@gmail.com>
2025-09-12 14:09:38 +0530

    program to check if 'a' is greater than or equal to 'b'

---
+++ Basics/check_greater_equal.py
@@ -0,0 +1,11 @@
+# Program to check if 'a' is greater than or equal to 'b'
+
+# Taking integer input for 'a' and 'b'
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+
+# Checking the condition: if a is greater than or equal to b
+if a >= b:
+    print("True")   # Prints True if condition is satisfied
+else:
+    print("False")  # Prints False if condition is not satisfied
