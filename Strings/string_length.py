f45d5e88df8b7d315836048b4e8feeffdad03e1c
Harsh Kumar <harsh.kumar9118b@gmail.com>
2025-09-18 11:27:35 +0530

    program to calculate length of user's first name

---
+++ Strings/string_length.py
@@ -0,0 +1,7 @@
+# Program to input user's first name and print its length
+
+# Take user input for first name
+first_name = input("Enter your first name: ")
+
+# Calculate and print the length of the first name using len()
+print("The length of your first name is:", len(first_name))
