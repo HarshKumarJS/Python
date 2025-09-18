b72b92d31426ebaee1cf20b025a84afad5d5e4aa
Harsh Kumar <harsh.kumar9118b@gmail.com>
2025-09-18 11:34:50 +0530

    Program to count occurrences of $ in a string

---
+++ Strings/count_dollar_occurrences.py
@@ -0,0 +1,10 @@
+# Program to find the number of occurrences of '$' in a string
+
+# Define the input string
+str1 = "A$$e$"
+
+# Use the count() method to count occurrences of '$'
+count = str1.count("$")
+
+# Print the result
+print("Number of occurrences of '$':", count)
