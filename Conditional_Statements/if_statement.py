3cf2dc762194cace9e52deb24e84d4d1807c3f67
Harsh Kumar <harsh.kumar9118b@gmail.com>
2025-09-19 13:03:48 +0530

    Program to check voting eligibility using if_statement

---
+++ Conditional_Statements/if_statement.py
@@ -0,0 +1,8 @@
+# Program to check voting eligibility
+
+# Assign the age of the person
+age = 20
+
+# Check if the person is 18 or older
+if age >= 18:
+    print("Eligible to vote.")  # Print eligibility message
