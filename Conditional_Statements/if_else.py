8e133b82764cdd73186e0ab3439432b0b3142921
Harsh Kumar <harsh.kumar9118b@gmail.com>
2025-09-19 13:05:55 +0530

    added program to check if a number is even or odd

---
+++ Conditional_Statements/if_else.py
@@ -0,0 +1,10 @@
+# Program to check whether a number is even or odd
+
+# Assign a number to check
+num = 7
+
+# Check divisibility by 2
+if num % 2 == 0:
+    print("The number is even.")   # If divisible by 2 → Even
+else:
+    print("The number is odd.")    # Otherwise → Odd
