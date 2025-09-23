286aff660fa2ff5e49074dfb6df704d53e92151b
Harsh Kumar <harsh.kumar9118b@gmail.com>
2025-09-23 15:47:09 +0530

    program to print elements of a list using a for loop

---
+++ Loops/print_list_elements.py
@@ -0,0 +1,8 @@
+# This program prints each element of the given list using a for loop
+
+# Define the list
+a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+# Iterate through each element in the list and print it
+for i in a:
+    print(i)
