27c64f37238fcbe45df6b849ada210c7d2974088
Harsh Kumar <harsh.kumar9118b@gmail.com>
2025-10-03 12:46:47 +0530

    program to count number of students with grade A in a tuple

---
+++ Tuple/count_grade_A.py
@@ -0,0 +1,10 @@
+# Program to count the number of students with grade "A" in a tuple
+
+# Tuple containing student grades
+t = ("C", "D", "A", "A", "B", "B", "A")
+
+# Count the number of times grade 'A' appears in the tuple
+count_a = t.count('A')
+
+# Print the result
+print(f"The total number of students with grade A is/are {count_a}")
